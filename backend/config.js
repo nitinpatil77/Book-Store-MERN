6878b20c41d1b5fa8f088e714fc8570e19dab622
nitinpatil77 <patilnitin1437@gmail.com>
2024-12-23 12:17:00 +0530

    chnages on mongoose

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
 
 export const PORT = 3535;
 
-export const mongoDBURL ='mongodb+srv://root:root@books-store-mern.3o8ah.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Books-store-mern';
+export const mongoDBURL ='mongodb+srv://root:root@cluster0.3o8ah.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
